e45ac5acdbcbd74571229c08b5e43dca239a47d8
santiagodls <itnas06@gmail.com>
2016-04-06 01:05:56 +0200

    Arreglos en mediaqueries y en htmls del menú

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-  $("#lenguaje").click(function(){
-       $(".submenu").stop().animate({height: "toggle"}, 500);
+    $("#lenguaje").click(function(){
+        if ($(window).width() < 767){
+            $(".submenu").stop().animate({width: "toggle"}, 500);
+        }else {
+            $(".submenu").stop().animate({height: "toggle"}, 500);
+        }
     });
     $("#iconomenu").click(function(){
         $(".menu").stop().animate({height: "toggle"}, 800);
