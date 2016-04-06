e22a2f6abbaa25048804fc362cd8d669b26c8203
Nicolas De los Santos <ndls05@gmail.com>
2016-04-06 23:51:43 +0200

    Corregidos enlaces en el menu de cambio de idioma

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var locale = window.location.pathname.split("/")[1];
+    var active_lang = $("#menu_lenguaje>li").each(function(i, e) {
+        if (e.childNodes[0].innerHTML.toLowerCase() === locale) {
+            $(e).attr("id", "lenguaje").removeClass("submenu");
+            $(e).children("a").removeAttr("href");
+        }
+    });
     $("#lenguaje").click(function(){
         if ($(window).width() < 767){
             $(".submenu").stop().animate({width: "toggle"}, 500);
