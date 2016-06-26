17f2182f6f5476a3e7fb1e13fe02e6732c2ef767
Nicolas De los Santos <ndls05@gmail.com>
2016-06-26 19:42:07 +0200

    Corregido error en el selector de idiomas que al cambiar a ingles ya no dejaba cambiar a español de nuevo.

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
-    $("#lenguaje").click(function(){
+    var path = window.location.href.toString().split(window.location.host)[1];
+    var lang = path.split('/')[1];
+    lang = lang.charAt(0).toUpperCase() + lang.slice(1);
+    var selected_lang = $("#menu_lenguaje :contains(" + lang + ")").first();
+    if (selected_lang.length > 0) {
+        console.log("Working");
+        $("#lenguaje").removeAttr('id').addClass('submenu'); 
+        selected_lang.attr('id', 'lenguaje').removeClass('submenu'); 
+        selected_lang.parent().prepend(selected_lang);
+    }
+    $("#lenguaje").click(function(e){
         if ($(window).width() < 767){
             $(".submenu").stop().animate({width: "toggle"}, 500);
         }else {
             $(".submenu").stop().animate({height: "toggle"}, 500);
         }
     });
+    $("#lenguaje a").click(function() {$("#lenguaje").click(); return false;});
     $("#iconomenu").click(function(){
         $(".menu").stop().animate({height: "toggle"}, 800);
     });
